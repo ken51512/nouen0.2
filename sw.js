e3957184dc07b93aef1a20ac8dcf878edee77cfe
ken51512 <kenxxxn40@gmail.com>
2026-07-17 17:21:58 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,20 @@
-const C='nouen-choja-v06';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./icon-192.png','./icon-512.png']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='nouen-choja-v07-20260717';
+const CORE=['./','./index.html','./icon-192.png','./icon-512.png','./manifest.json'];
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('message',event=>{if(event.data?.type==='SKIP_WAITING')self.skipWaiting()});
+self.addEventListener('fetch',event=>{
+  if(event.request.mode==='navigate'){
+    event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{
+      const copy=response.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return response;
+    }).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+    const copy=response.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return response;
+  })));
+});
